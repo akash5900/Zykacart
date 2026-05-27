f798bc07548f5504ecc51fad8bf6d2e412bbc128
akash5900 <singhakash517408@gmail.com>
2026-05-27 14:33:32 +0530

    fixed feature

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -1,17 +1,17 @@
 function Features() {
   return (
     <div className="border bg-pink-50 p-2 md:p-4">
-      <div className="border md:p-2 flex md:flex-row items-center justify-center gap-2 md:gap-20 rounded bg-white">
+      <div className=" border p-2 md:p-2 flex md:flex-row items-center justify-center gap-1 md:gap-20 rounded bg-white">
         <div>
-          <span className="text-[13px]">7 Days Easy Return</span>
+          <span className="text-[12px]">7 Days Easy Return</span>
         </div>
         <div className=" h-3 md:h-6 w-px bg-gray-300"></div>
         <div>
-          <span className="text-[13px]">Cash on Delivery</span>
+          <span className="text-[12px]">Cash on Delivery</span>
         </div>
         <div className="h-3 md:h-6 w-px bg-gray-300"></div>
         <div>
-          <span className="text-[13px]">Lowest Prices</span>
+          <span className="text-[12px]">Lowest Prices</span>
         </div>
       </div>
     </div>
