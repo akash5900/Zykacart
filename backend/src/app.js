4e51a3103ffe242ab52f6e32eb4d0b214912fb2e
akash5900 <singhakash517408@gmail.com>
2026-05-27 12:27:55 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: ["https://zykacart.vercel.app"],
+    origin: ["https://zykacart.vercel.app", "https://zykacart-ufpv.vercel.app"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
