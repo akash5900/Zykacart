06a7d3265ba8046485ab4bc8aa772932ecf13cdf
akash5900 <singhakash517408@gmail.com>
2026-05-28 11:21:26 +0530

    fixed frontend

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -3,7 +3,7 @@ function Features() {
     <div className="border bg-pink-50 p-2 md:p-4">
       <div className=" border p-2 md:p-2 flex md:flex-row items-center justify-center gap-2 md:gap-20 rounded bg-white">
         <div>
-          <span className="text-[12px] md:text:md">7 Days Easy Return</span>
+          <span className="text-[12px] md:text:lg">7 Days Easy Return</span>
         </div>
         <div className=" h-3 md:h-6 w-px bg-gray-300"></div>
         <div>
