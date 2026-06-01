874f5831edf3c13b6ecd9559b3b949d74743ee61
akash5900 <singhakash517408@gmail.com>
2026-06-01 17:39:47 +0530

    fixed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,7 +10,6 @@ import { useState } from "react";
 
 const Footer = () => {
   const navigate = useNavigate();
-  const user = JSON.parse(localStorage.getItem("user"));
   const [email, setEmail] = useState("");
 
   const handleSeller = () => {
